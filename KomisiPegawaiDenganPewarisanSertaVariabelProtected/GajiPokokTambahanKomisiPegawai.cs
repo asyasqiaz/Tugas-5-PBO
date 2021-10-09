42f0e807f9cde8426f9f1ca6fbe2a087e13e763c
Annisa Syasqia Zain <89344664+asyasqiaz@users.noreply.github.com>
2021-10-09 20:45:17 +0700

    Add files via upload

---
+++ KomisiPegawaiDenganPewarisanSertaVariabelProtected/GajiPokokTambahanKomisiPegawai.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KomisiPegawaiDenganPewarisan
+{
+    public class GajiPokokTambahanKomisiPegawai : KomisiPegawai
+    {
+        
+
+        private decimal gajiPokok; // gaji pokok mingguan
+
+        // konstruktor kelas turunan 6 parameter
+        // dengan memanggil ke konstruktor kelas induk CommissionEmployee
+
+        public GajiPokokTambahanKomisiPegawai(string namaAwal, string namaAkhir, string nomorKTP, decimal penjualanKotor, decimal tingkatKomisi, decimal gajiPokok) : base(namaAwal, namaAkhir, nomorKTP, penjualanKotor, tingkatKomisi)
+        {
+            GajiPokok = gajiPokok; // memvalidasi gaji pokok
+        }
+
+        // properti untuk mendapatkan dan menetapkan nilai
+        // gaji pokok kelas GajiPokokTambahanKomisiPegawai
+        public decimal GajiPokok
+        {
+            get { return gajiPokok; }
+            set
+            {
+                if (value < 0) // validasi
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GajiPokok)} pasti >= 0");
+                }
+                gajiPokok = value;
+            }
+        }
+
+        // perhitungan penghasilan
+        public override decimal Penghasilan() => gajiPokok + (tingkatKomisi * penjualanKotor);
+
+        // mengembalikan representasi string dari objek KomisiPegawai
+        // tidak dibolehkan mengakses anggota private kelas induk 
+        public override string ToString() =>
+            $"Pegawai dengan komisi gaji pokok : {NamaAwal} {NamaAkhir}\n" + $"Nomor KTP : {NomorKTP}\n" + $"Penjualan kotor : {penjualanKotor}\n" + $"Tingkat komisi : {tingkatKomisi:F2}\n" + $"Gaji pokok : {gajiPokok:C}";
+        
+    }
+}
---
+++ KomisiPegawaiDenganPewarisanSertaVariabelProtected/KomisiPegawai.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class KomisiPegawai : object
+    {
+        public string NamaAwal { get; }
+        public string NamaAkhir { get; }
+        public string NomorKTP { get; }
+        protected decimal penjualanKotor; // penjualan kotor mingguan
+        protected decimal tingkatKomisi; // persentase komisi
+
+        // 5 parameter konstruktor
+        public KomisiPegawai(string namaAwal, string namaAkhir, string nomorKTP, decimal penjualanKotor, decimal tingkatKomisi)
+        {
+            // panggilan implisit ke konstruktor objek terjadi di bagian ini
+            NamaAwal = namaAwal;
+            NamaAkhir = namaAkhir;
+            NomorKTP = nomorKTP;
+            PenjualanKotor = penjualanKotor;
+            TingkatKomisi = tingkatKomisi;
+        }
+
+        // properti yang mendapatkan dan menetapkan komisi penjualan kotor pegawai
+        public decimal PenjualanKotor
+        {
+            get { return penjualanKotor; }
+            set
+            {
+                if (value < 0) // validasi
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(PenjualanKotor)} pasti >= 0");
+                }
+                penjualanKotor = value;
+            }
+        }
+
+        // properti yang mendapatkan dan menetapkan tingkat komisi pegawai
+        public decimal TingkatKomisi
+        {
+            get { return tingkatKomisi; }
+            set
+            {
+                if (value <= 0 || value >= 1) // validasi
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(TingkatKomisi)} pasti > 0 and < 1");
+                }
+                tingkatKomisi = value;
+            }
+        }
+
+        // perhitungan penghasilan komisi pegawai
+        public virtual decimal Penghasilan()
+        {
+            return tingkatKomisi * penjualanKotor;
+        }
+
+        // mengembalikan representasi string dari objek KomisiPegawai
+        public override string ToString()
+        {
+            return $"Pegawai dengan komisi : {NamaAwal} {NamaAkhir}\n" + $"Nomor KTP : {NomorKTP}\n" + $"Penjualan kotor : {PenjualanKotor}\n" + $"Tingkat komisi : {TingkatKomisi:F2}";
+        }
+    
+}
